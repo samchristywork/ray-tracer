b8d3922c88859d466819435fec6ecdb3207de1dd
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-09-30 21:43:40 -0500

    feat: Add basic utility operations
    These functions are used to perform operations on vectors. Also have a
    clamp function so we don't have to worry as much about integer overflow.

--- src/main.c
+++ src/main.c
@@ -31,6 +31,45 @@ typedef struct {
     float reflection;
 } Triangle;
 
+Vec3 vec3(float x, float y, float z) {
+    return (Vec3){x, y, z};
+}
+
+Vec3 vec_add(Vec3 a, Vec3 b) {
+    return vec3(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+Vec3 vec_sub(Vec3 a, Vec3 b) {
+    return vec3(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+Vec3 vec_scale(Vec3 a, float s) {
+    return vec3(a.x * s, a.y * s, a.z * s);
+}
+
+float vec_dot(Vec3 a, Vec3 b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+Vec3 vec_norm(Vec3 a) {
+    float mag = sqrtf(vec_dot(a, a));
+    return vec_scale(a, 1.0f / mag);
+}
+
+Vec3 vec_cross(Vec3 a, Vec3 b) {
+    return vec3(
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x
+    );
+}
+
+float clamp(float x, float lower, float upper) {
+    if (x < lower) return lower;
+    if (x > upper) return upper;
+    return x;
+}
+
 int main() {
     unsigned char *image = malloc(IMAGE_WIDTH * IMAGE_HEIGHT * 3);
     if (image == NULL) {
