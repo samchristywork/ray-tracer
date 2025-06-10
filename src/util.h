62bb548c14d0cb44cca23b4f893d51bd72f222bd
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-06-10 17:52:33 -0500

    chore: Remove all old code so that we can start again

--- src/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTIL_H
-#define UTIL_H
-
-float clamp(float x, float lower, float upper);
-
-void save_image_to_file(const unsigned char *image, const char *filename,
-                        int width, int height);
-
-#endif
